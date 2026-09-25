1424942e1f4fedb1baa1df70274f2a6a03790f1d
Eryk Grzesiak <email>
2026-09-25 09:43:46 +0200

    'zadania002'

--- Zadanka2/src/Main.java
+++ Zadanka2/src/Main.java
@@ -1,13 +1,51 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+//        //zad 1
+//        Scanner scanner = new Scanner((System.in));
+//        System.out.println("Wpisz dowolna liczbę aby sprawdzić czy jest podzielna przez 3");
+//
+//        int liczba = scanner.nextInt();
+//
+//        if (liczba % 3 == 0) {
+//            System.out.println("Liczba jest podzielna przez 3");
+//        } else {
+//            System.out.println("Liczba nie jest podzielna przez 3");
+//        }
+        //zad 2
+        System.out.println("Podaj pierwsza liczbe");
+        Scanner trojkat = new Scanner(System.in);
+        int a = trojkat.nextInt();
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+        System.out.println("Podaj druga liczbe");
+        int b = trojkat.nextInt();
+
+        System.out.println("Podaj trzecia liczbe");
+        int c = trojkat.nextInt();
+
+        if (a + b > c && a + c > b && b + c > a) {
+                System.out.println("Mozna zbudowac trojkat.");
+        } else {
+                System.out.println("Nie mozna zbudowac trojkata.");
+        }
+
+        //zad 3
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj pierwszą liczbę: ");
+        double d = scanner.nextDouble();
+
+        System.out.print("Podaj drugą liczbę: ");
+        double f = scanner.nextDouble();
+
+        if (d > f) {
+                System.out.println("Największa liczba to: " + d);
+        } else if (f > d) {
+                System.out.println("Największa liczba to: " + f);
+        } else {
+                System.out.println("Liczby są równe: " + d);
+        }
 }
+
+
